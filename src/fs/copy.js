93e627fbe9190960908cd5c15aea2dff51547d0d
banzayche <dharm.serg@gmail.com>
2023-06-27 00:39:46 +0300

    Node.js basics (#1)
    * Finishing with create and copy
    
    * Minor changes
    
    * Finishing with fs tasks
    
    * Finishing with cli tasks
    
    * finishing with esm
    
    * finishing with hash
    
    * Finishing with streams
    
    * Finishing with zip
    
    * Finishing with workers
    
    * Minor fixes
    
    * Finishing with child process

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import { existsSync } from 'node:fs';
+import { readdir, copyFile, mkdir } from 'node:fs/promises'
+
 const copy = async () => {
-    // Write your code here 
+    // Write your code here
+  const __basicsDir = new URL(".", import.meta.url).pathname;
+  const filesDir = `${__basicsDir}files/`;
+  const filesCopyDir = `${__basicsDir}files_copy/`;
+
+  try {
+    if (!existsSync(filesDir) || existsSync(filesCopyDir)) throw Error('FS operation failed');
+
+    await mkdir(filesCopyDir, { recursive: true });
+
+    const files = await readdir(filesDir);
+    for (const file of files) {
+      await copyFile(`${filesDir}${file}`, `${filesCopyDir}${file}`);
+    }
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import { existsSync } from 'node:fs';
+import { appendFile } from 'node:fs/promises'
+
 const create = async () => {
-    // Write your code here 
+    // Write your code here
+    const __basicsDir = new URL(".", import.meta.url).pathname;
+    const path = `${__basicsDir}files/fresh.txt`;
+    try {
+        if (existsSync(path)) throw Error('FS operation failed');
+        await appendFile(path, 'I am fresh and young', { encoding: 'utf8' });
+    } catch (e) {
+        console.error(e);
+    }
 };
 
 await create();
